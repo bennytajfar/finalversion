6e612b1b35450ee0b7118eee49d8a0dd820f320c
bennytajfar <62610682+bennytajfar@users.noreply.github.com>
2020-10-03 16:32:27 -0500

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Header from './Header'
+import './Main.css'
+import Signin from './Signin'
+import Logo from './logo.png'
+
+const Main = () => {
+    return (
+        <div className='main'>
+            <Header/>
+            <div className='skul'>
+                <img className='skullogo' src={Logo} alt='logo'/>
+            </div>
+            <Signin/>
+        </div>
+    )
+}
+import React from 'react'
+import Header from './Header'
+import './Main.css'
+import Signin from './Signin'
+import Logo from './logo.png'
+
+const Main = () => {
+    return (
+        <div className='main'>
+            <Header/>
+            <div className='skul'>
+                <img className='skullogo' src={Logo} alt='logo'/>
+            </div>
+            <Signin/>
+        </div>
+    )
+}
+
+export default Main
+
+export default Main
