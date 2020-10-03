d80442e0f350d0cb3775407f6937ff8be7ad2ce3
bennytajfar <62610682+bennytajfar@users.noreply.github.com>
2020-10-03 16:34:10 -0500

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header__logo'>
+                Benny's Universe
+            </div>
+            <div className='header__text'>
+                we will always protect your privacy
+            </div>
+        </div>
+    )
+}
+
+export default Header
